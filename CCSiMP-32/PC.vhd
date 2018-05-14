b35e373992dfbf73774859cb3a40183e56326322
Rafael Li Chen <rafaellichen@gmail.com>
2018-05-14 14:54:01 -0400

    add PC.vhd

--- CCSiMP-32/PC.vhd
+++ CCSiMP-32/PC.vhd
@@ -1,26 +1,22 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity PC is
-    Port ( I_PC : in  STD_LOGIC_VECTOR (31 downto 0);
+    Port ( I_PC_UPDATE: in STD_LOGIC;
+	        I_PC : in  STD_LOGIC_VECTOR (31 downto 0);
            O_PC : out  STD_LOGIC_VECTOR (31 downto 0));
 end PC;
 
 architecture Behavioral of PC is
+	signal CUR_PC : std_logic_vector (31 downto 0) := (others => '0') ;
 
 begin
-	process (I_PC)
+	process (I_PC_UPDATE, I_PC)
 	begin
-		O_PC <= I_PC;
+		if I_PC_UPDATE = '1' then
+			CUR_PC <= I_PC;			
+		end if;
+		O_PC <= CUR_PC;
 	end process;
 
 end Behavioral;
