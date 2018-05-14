f21e39ae14d5732fee2d9ad4546de9b64713a690
kiseki123 <zhangbaikeng@gmail.com>
2018-05-14 16:37:28 -0400

    create symboles for all modules

--- CCSiMP-32/ADD1.vhd
+++ CCSiMP-32/ADD1.vhd
@@ -4,15 +4,15 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity ADD1 is
     Port ( I_ADD1_A : in  STD_LOGIC_VECTOR (31 downto 0);
-           O_ADD1_OUT : out  STD_LOGIC_VECTOR (31 downto 0));
+           O_ADD1_Out : out  STD_LOGIC_VECTOR (31 downto 0));
 end ADD1;
 
 architecture Behavioral of ADD1 is
 
-	signal count : STD_LOGIC_VECTOR (3 downto 0) := "0100";
+	signal count : STD_LOGIC_VECTOR (3 downto 0) := "0001";
 
 begin
-	O_ADD1_OUT <= I_ADD1_IN + count;
+	O_ADD1_Out <= I_ADD1_A + count;
 
 end Behavioral;
 
