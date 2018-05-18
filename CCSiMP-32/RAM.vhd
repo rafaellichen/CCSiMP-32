7cd9939fd6bd229fe63b2455e56233694002643d
Rafael Li Chen <rafaellichen@gmail.com>
2018-05-18 16:44:36 -0400

    changed to 8 bits per element for RAM

--- CCSiMP-32/RAM.vhd
+++ CCSiMP-32/RAM.vhd
@@ -13,18 +13,23 @@ end RAM;
 
 architecture Behavioral of RAM is
 
-	type buf32x256 is array (0 to 255) of STD_LOGIC_VECTOR (31 downto 0);
-	signal ram : buf32x256 := (others => x"00000000");
-	signal temp : std_logic_vector(31 downto 0) := x"00000000";
+	type buf32x256 is array (0 to 255) of STD_LOGIC_VECTOR (7 downto 0);
+	signal ram : buf32x256 := (others => x"00");
+	signal temp : std_logic_vector(7 downto 0) := x"00";
+	signal addr : integer := 0;
 	
 begin
 	process (I_RAM_EN, I_RAM_RE, I_RAM_WE, I_RAM_ADDR)
 	begin
+		addr <= to_integer(unsigned(I_RAM_ADDR));
 		if I_RAM_EN = '1' then
 			if I_RAM_RE = '1' then
-				O_RAM_DATA <= ram(to_integer(unsigned(I_RAM_ADDR)));
+				O_RAM_DATA <= ram(addr) & ram(addr+1) & ram(addr+2) & ram(addr+3);
 			elsif I_RAM_WE = '1' then
-				ram(to_integer(unsigned(I_RAM_ADDR))) <= I_RAM_DATA;
+				ram(addr+3) <= I_RAM_DATA(7 downto 0);
+				ram(addr+2) <= I_RAM_DATA(15 downto 8);
+				ram(addr+1) <= I_RAM_DATA(23 downto 16);
+				ram(addr) <= I_RAM_DATA(31 downto 24);
 			end if;
 		end if;
 	end process;
